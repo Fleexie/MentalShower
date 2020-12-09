f125409f173c452be7f8e82e1c5437b221040166
Fleexie <jannickolsendk@hotmail.com>
2020-12-09 21:19:26 +0100

    Dialog option for the settings buttons. Minor styling.

--- src/app/fan-setting/fan-basic/fan-basic.component.ts
+++ src/app/fan-setting/fan-basic/fan-basic.component.ts
@@ -1,5 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core';
 import { faSnowflake, faSun, faThumbsUp} from '@fortawesome/free-solid-svg-icons';
+import { MatDialog, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+
 
 @Component({
   selector: 'app-fan-basic',
@@ -12,9 +14,63 @@ export class FanBasicComponent implements OnInit {
   cold = faSnowflake;
   warm = faSun;
   good = faThumbsUp;
-  constructor() { }
+
+  currentFan;
+  constructor(public dialog: MatDialog) { }
+
+  /* Dialog method */
+  coldClicked(parameter): void{
+    this.currentFan = parameter;
+    let settingType;
+    /* Switch to determine what setting has been clicked */
+    switch (this.currentFan) {
+      case 'coldSetting':
+        settingType = 'Cold';
+        break;
+        case 'goodSetting':
+          settingType = 'Good';
+          break;
+          case 'warmSetting':
+            settingType = 'Warm';
+            break;
+      }
+      /* Refer to the dialog */
+    const dialogRef = this.dialog.open(ColdDialogComponent, {
+          width: '80%',
+          height: 'fit-content',
+          hasBackdrop: true,
+          panelClass: ['dialogClass'],
+          data: { settings: settingType }
+      });
+
+    dialogRef.afterClosed().subscribe(result => {
+          console.log('The dialog was closed');
+      });
+  }
 
   ngOnInit(): void {
   }
+
+}
+
+
+/* Dialog component */
+@Component({
+    selector: 'app-dialog-cold',
+    template: `<h2>You clicked that you are feeling <span class="{{data.settings}}">{{data.settings}}</span>.</h2>
+    <p>The system will take care of any changes to the ventilation unit.</p>
+    <p>You may not feel a change immediately</p>
+`,
+    styleUrls: ['fan-basic.component.css']
+})
+export class ColdDialogComponent {
+    constructor(
+        public dialogRef: MatDialogRef<FanBasicComponent>,
+        @Inject(MAT_DIALOG_DATA) public data: {settings: string}) {
+
+    }
+    onNoClick(): void {
+        this.dialogRef.close();
+    }
 
 }
