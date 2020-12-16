fcc072769030f2f028dc1581349462d0d8a21082
Deepak Gautam <anin0543@edu.easj.dk>
2020-12-16 01:02:02 +0100

    learning-element quiz

--- src/app/info/info.component.ts
+++ src/app/info/info.component.ts
@@ -1,23 +1,23 @@
 import { Component, OnInit } from '@angular/core';
-import { faFan } from '@fortawesome/free-solid-svg-icons';
 import { ActivatedRoute, Router } from '@angular/router';
+import { faFan } from '@fortawesome/free-solid-svg-icons';
 @Component({
   selector: 'app-info',
   templateUrl: './info.component.html',
-  styleUrls: ['./info.component.css']
+  styleUrls: ['./info.component.css'],
 })
 export class InfoComponent implements OnInit {
-  currentSection = 0;
-  fan = faFan;
+  public currentSection = 0;
+  public fan = faFan;
   constructor( private router: Router) { }
 
-  ngOnInit(): void {
+  public ngOnInit(): void {
   }
-  changeSection(delta: number): void {
+  public changeSection(delta: number): void {
     // some checks
     this.currentSection += delta;
   }
-  goToquiz(){
+  public goToquiz() {
   this.router.navigate(['quiz']);
   }
 }
